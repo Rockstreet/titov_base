54a3c72c772a0b41b25a570953baba4af2ce699e
ivan.tolkachev@gmail.com <ivan.tolkachev@gmail.com>
2017-04-06 22:06:53 +0300

    Не работают разделы

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django_mptt_admin.admin import DjangoMpttAdmin
+from .models import Category
+from embed_video.admin import AdminVideoMixin
 
+
+from .models import Category
 # Register your models here.
+
+
+class CategoryAdmin(admin.ModelAdmin):
+
+    inlines = [ ]
+
+admin.site.register(Category, CategoryAdmin)
--- base/models.py
+++ base/models.py
@@ -6,18 +6,18 @@ from embed_video.fields import EmbedVideoField
 from sorl.thumbnail import ImageField
 
 
-
-
 class Category(models.Model):
-    created_date = models.DateTimeField(_("Дата создания"), auto_now_add=True, editable=False)
-    edited_date = models.DateTimeField(_("Дата редактирования"), auto_now=True, editable=False, null=True)
-    title = models.CharField(_("Название"), max_length=1000, default='')
+    created_date = models.DateTimeField("Дата создания", auto_now_add=True, editable=False)
+    edited_date = models.DateTimeField("Дата редактирования", auto_now=True, editable=False, null=True)
+    title = models.CharField("Название", max_length=1000, default='')
     content = RichTextField("Описание", blank=True)
 
     def __str__(self):
         return self.title
 
     class Meta:
-        verbose_name = _("Раздел")
-        verbose_name_plural = _("Раздел")
+        verbose_name ="Раздел"
+        verbose_name_plural = "Раздел"
         ordering = ['title', ]
+
+
