15f6e408c7cb55c8db3b16597854f66b00b48073
Rock <ivan.tolkachev@gmail.com>
2017-04-07 17:44:55 +0300

    Добавлены регионы

--- base/admin.py
+++ base/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django_mptt_admin.admin import DjangoMpttAdmin
-from .models import Category,Item
+from .models import Category,Item,Reg
 from embed_video.admin import AdminVideoMixin
 
 
@@ -16,8 +16,16 @@ class ItemAdmin(admin.ModelAdmin):
 
     inlines = [ ]
 
+class RegAdmin(admin.ModelAdmin):
+
+    inlines = [ ]
+
 admin.site.register(Category, CategoryAdmin)
 
 admin.site.register(Item, ItemAdmin)
+
+admin.site.register(Reg, RegAdmin)
+
+
 
 
--- base/models.py
+++ base/models.py
@@ -16,8 +16,21 @@ class Category(models.Model):
         return self.title
 
     class Meta:
-        verbose_name = _("Разделы")
+        verbose_name = _("Раздел")
         verbose_name_plural = _("Разделы")
+        ordering = ['title', ]
+
+class Reg(models.Model):
+    created_date = models.DateTimeField(_("Дата создания"), auto_now_add=True, editable=False)
+    edited_date = models.DateTimeField(_("Дата редактирования"), auto_now=True, editable=False, null=True)
+    title = models.CharField(_("Наименование"), max_length=1000, default='')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = _("Регион")
+        verbose_name_plural = _("Регионы")
         ordering = ['title', ]
 
 
@@ -25,6 +38,7 @@ class Item(models.Model):
     title = models.CharField(_("Название объекта"), max_length=1000, default='')
     num = models.IntegerField(_("Порядковый номер"), default=0, blank=True, db_index=True)
     categories = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True)
+    regions = models.ForeignKey(Reg, on_delete=models.CASCADE, blank=True, null=True)
     content = RichTextField("Описание", blank=True)
 
     class Meta:
